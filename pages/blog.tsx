8afd893a6fc2864ad506327b319c10cba0ec7f0d
Janzen Faidiban <janzenfaidiban@gmail.com>
2021-11-12 22:50:20 +0900

    Data Fetching getServerSideProps

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,10 +1,42 @@
-import Header from '../components/Header';
 import Layout from '../components/Layout';
+import styles from '../styles/Blog.module.css';
 
-export default function Blog() {
+interface Post {
+    id: number;
+    title: string;
+    body: string;
+}
+
+interface BlogProps {
+    dataBlog: Post[];
+}
+
+export default function Blog(props: BlogProps) {
+
+    const { dataBlog } = props;
+
     return (
-        <Layout pageTitle="Blog">
-            <h1 className="title">Blog</h1>
+        <Layout pageTitle="Blog" >
+            {dataBlog.map(blog => {
+                return (
+                    <div key={blog.id} className={styles.card}>
+                        <h3>{blog.title}</h3>
+                        <p>{blog.body}</p>
+                    </div>
+                )
+            })};
         </Layout>
     )
+}
+
+export async function getServerSideProps() {
+
+    const res = await fetch('http://jsonplaceholder.typicode.com/posts');
+    const dataBlog = await res.json();
+
+    return {
+        props: {
+            dataBlog,
+        },
+    };
 }
